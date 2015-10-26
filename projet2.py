666112b240abc4dafd76ac163839ecbd986c1a7e
t00n <kelkununtel@hotmail.com>
2015-10-27 00:07:20 +0100

    3 functions ok

--- projet2.py
+++ projet2.py
@@ -7,29 +7,15 @@ __author__ = 'Noël Neri'
 from turtle import *
 from math import radians,cos,floor
 
-EPS = 1.0e-5
 FACTEUR = cos(radians(30))
 
 entree = lambda valeur, default, mini='', maxi='', typ=int: (lambda x: default if x == '' else x if typ == str else mini if typ(x) < mini else maxi if typ(x) > maxi else typ(x))(input(str(valeur)+"["+str(default)+"]?> "))
 
-deform = lambda p: (lambda p,c,r:(lambda d: p if d >= r else (lambda facteur:[c[i]+(p[i]-c[i])*facteur for i in range(3)]) (r/d if d > EPS else 1)) (sum([(c[i]-p[i])**2 for i in range(3)])**0.5))(p, centre, r)
+deform = lambda p: (lambda p,c,r:(lambda d: p if d >= r else (lambda facteur:[c[i]+(p[i]-c[i])*facteur for i in range(3)]) (r/d if d > 1.0e-5 else 1)) (sum([(c[i]-p[i])**2 for i in range(3)])**0.5))(p, centre, r)
 
-def hexagone(t, c, longueur, col1, col2, col3, deform, chemin=[[(1,0),(0.5,FACTEUR),(-0.5,FACTEUR)],[(1,0),(0.5,-FACTEUR),(-0.5,-FACTEUR)],[(-0.5,-FACTEUR),(-1,0),(-0.5,FACTEUR)]]):
-    """reçoit la tortue t, le centre c, la distance longueur, les trois couleurs col[1-3]
-    et la fonction importée deform, dessine un hexagone composé de trois losanges de couleurs
-    différentes et et utilise la fonction deform sur chaqu'un des points déssinés
-    """
-    COULEURS=(col1,col2,col3) # Triplet des couleurs
-    t.pu(),t.goto(c[0],c[1])
-    for p,colo in enumerate(COULEURS):
-        t.goto(deform((c[0],c[1],0))[:2]),t.color(colo,colo),t.fillcolor(colo),t.pd(),t.begin_fill(),[t.goto(deform((c[0]+(round(i[0]*longueur,2)),c[1]+(round(i[1]*longueur,2)),0))[:2]) for i in chemin[p]],t.end_fill()
+hexagone = lambda t, c, longueur, col1, col2, col3, deform: (t.pu(),t.goto(c[0],c[1]),[(t.goto(deform((c[0],c[1],0))[:2]),t.color(colo,colo),t.fillcolor(colo),t.pd(),t.begin_fill(),[t.goto(deform((c[0]+(round(i[0]*longueur,2)),c[1]+(round(i[1]*longueur,2)),0))[:2]) for i in [[(1,0),(0.5,FACTEUR),(-0.5,FACTEUR)],[(1,0),(0.5,-FACTEUR),(-0.5,-FACTEUR)],[(-0.5,-FACTEUR),(-1,0),(-0.5,FACTEUR)]][p]],t.end_fill()) for p,colo in enumerate((col1,col2,col3))])
 
 def pavage(t, inf_gauche, sup_droit, longueur, col1, col2, col3, deform):
-    """Prends la tortue t, le coin inférieur gauche et le coin supérieur droit de la fenêtre,
-    la longueur des cotés de l'hexagone (utilisée pour disposer la grille)
-    les trois couleurs et la fonction deform pour les passer à la fonction hexagone.
-    Dessine une grille composée d'hexagone disposés en quinconce
-    """
     imp = 1
     for i in range(inf_gauche,sup_droit,floor(longueur*FACTEUR)): # Rangées
         if imp == 1:
